18b0e9846fe71b18eb958e0cec427644134eed13
lo2495 <lohiyeung@gmail.com>
2026-07-04 19:47:26 +0800

    Added Readme file

--- daily_checkin.py
+++ daily_checkin.py
@@ -38,7 +38,7 @@ def load_cookies(driver, filepath):
 
 def run():
     options = uc.ChromeOptions()
-    # options.add_argument("--headless=new")          
+    options.add_argument("--headless=new")          
     options.add_argument("--no-sandbox")             
     options.add_argument("--disable-dev-shm-usage")  
     options.add_argument("--window-size=1920,1080")
